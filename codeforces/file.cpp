d51e1434c5bfd7d57d722320d19e7f3a978d7cd7
RohanK22 <rohank2205@gmail.com>
2021-05-24 17:00:30 +0530

    Added cpp template

---
+++ codeforces/file.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fo(i, n) for(i=0;i<n;i++)
+
+//^B for run
+
+void solve() {
+
+	// Write your solution here
+	
+	cout<<endl;
+}
+
+int main() {
+	int cases;
+	cin>>cases;
+	for(int c = 0; c< cases; c++) {
+		solve();
+	}
+}
