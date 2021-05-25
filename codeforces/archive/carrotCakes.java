e98bf8054121580858eb35bbe40cc412c9f42463
RohanK22 <rohank2205@gmail.com>
2021-05-25 15:38:50 +0530

    q

--- codeforces/archive/carrotCakes.java
+++ codeforces/archive/carrotCakes.java
@@ -1,7 +1,9 @@
+package archive;
+
 import java.io.*;
 import java.util.*;
 
-public class template {
+public class carrotCakes {
     public static Scanner sc  = null;
     public static FileWriter op = null;
     
