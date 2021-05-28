98ef503e5038a17865b8d398dd48f4053b1fe66b
RohanK22 <rohank2205@gmail.com>
2021-05-28 13:01:56 +0530

    q

---
+++ codeforces/archive/longestUncommonSubsequence.java
@@ -0,0 +1,148 @@
+import java.io.*;
+import java.util.*;
+
+public class template {
+    public static Scanner sc  = null;
+    public static FileWriter op = null;
+    
+    // set
+    public static boolean debug = false;
+    public static boolean casesPresent = false;
+    
+
+    public static void solve() throws Exception {
+        // Your solution here
+        String a = gs(), b= gs();// Assume a is larger in length
+        long max1 = max(a.length(), b.length());
+        if(a.equals(b)) println("-1");
+        else println(max1);
+    }
+
+
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        if(debug) {
+            sc = new Scanner(new File("input.txt"));
+            op = new FileWriter("output.txt");
+        } else {
+            sc = new Scanner(System.in);
+        }
+
+        if(casesPresent) {
+            int cases = gi();
+            gs();
+            for(int c = 0; c< cases; c++) 
+                solve();
+        } else {
+            solve();
+        }
+        
+        if(op != null) op.close();
+        sc.close();
+    }
+
+    // Max and min
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    public static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+
+    // Printing
+    public static void println(String str) throws Exception {
+        if(debug) {
+            op.write(str + "\n");
+        } else {
+            System.out.println(str);
+        }
+    }
+
+    public static void print(String str) throws Exception {
+        if(debug) {
+            op.write(str);
+        } else {
+            System.out.print(str);
+        }
+    }
+
+    
+    public static void println(long str) throws Exception {
+        if(debug) {
+            op.write(str + "\n");
+        } else {
+            System.out.println(str);
+        }
+    }
+
+    public static void print(long str) throws Exception {
+        if(debug) {
+            op.write(str + "\n");
+        } else {
+            System.out.println(str);
+        }
+    }
+
+    public static void print(int str) throws Exception {
+        if(debug) {
+            op.write(str);
+        } else {
+            System.out.print(str);
+        }
+    }
+
+    public static void println(float str) throws Exception {
+        if(debug) {
+            op.write(str + "\n");
+        } else {
+            System.out.println(str);
+        }
+    }
+
+    public static void print(float str) throws Exception {
+        if(debug) {
+            op.write(Float.toString(str));
+        } else {
+            System.out.print(str);
+        }
+    }
+
+    // Getting user input
+    public static int gi() {
+        return sc.nextInt();
+    }
+
+    public static long gl() {
+        return sc.nextLong();
+    }
+
+    public static float gf() {
+        return sc.nextFloat();
+    }
+
+    public static int[] gia() {
+        String[] sp = sc.nextLine().trim().split(" ");
+        int[] arr = new int[sp.length];
+        for(int i = 0; i< sp.length; i++) {
+            arr[i] = Integer.parseInt(sp[i]);
+        }
+        return arr;
+    }
+
+    public static float[] gfa() {
+        String[] sp = sc.nextLine().trim().split(" ");
+        float[] arr = new float[sp.length];
+        for(int i = 0; i< sp.length; i++) {
+            arr[i] = Float.parseFloat(sp[i]);
+        }
+        return arr;
+    }
+
+    public static String gs() {
+        return sc.nextLine();
+    }
+}
