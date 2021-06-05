5ada6cb292d89921afe6b65d64a94964a2cc958b
RohanK22 <rohank2205@gmail.com>
2021-06-05 10:39:50 +0530

    Adding a CPP template

---
+++ cpp/basic.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ar array
+#define vt vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define endl "\n"
+
+void solve() {
+	int a,b; 
+	string s;
+	cin>>a>>b>>s;
+	cout<<a<<b<<s;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t=1;
+	// cin >> t;
+	for(int i = 0; i < t; i++) {
+		// cout<<"Case #" <<(i+1)<<": "<<endl;
+		solve();
+	}
+	return 0;
+}
