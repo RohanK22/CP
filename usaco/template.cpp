b1043e73ba00e8235025764e7098af9686d140bc
RohanK22 <rohank2205@gmail.com>
2021-10-05 23:21:43 +0530

    Initial Commit USACO

---
+++ usaco/template.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // freopen("template.in", "r", stdin);
+    // freopen("template.out", "w", stdout);
+
+    string s;
+    cin>>s;
+    cout<<s;
+}
