ae8e10f3cf7f702c6759905b72304d602569ebea
RohanK22 <rohank2205@gmail.com>
2022-05-11 12:30:09 +0530

    Test

--- template.cpp
+++ template.cpp
@@ -175,7 +175,7 @@ const int d4i[4]={-1, 0, 1, 0}, d4j[4]={0, 1, 0, -1};
 const int d8i[8]={-1, -1, 0, 1, 1, 1, 0, -1}, d8j[8]={0, 1, 1, 1, 0, -1, -1, -1};
 
 void solve() {
-	;
+	printf("Hello");
 }
 
 int main() {
